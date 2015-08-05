9e36a77a244da71ff059a9d65137486872193405
Sebastian Wenninger <sebastian.wenninger@tum.de>
2015-08-05 16:29:20 +0200

    Merge

--- TUM.TT/TT.Viewer/ViewModels/ShellViewModel.cs
+++ TUM.TT/TT.Viewer/ViewModels/ShellViewModel.cs
@@ -11,18 +11,20 @@ namespace TT.Viewer.ViewModels {
 
         public ShellViewModel()
         {
+            this.DisplayName = "TUM.TT";
             FilterView = new FilterViewModel();
             MediaView = new MediaViewModel();
             ActivateItem(FilterView);
+            ActivateItem(MediaView);
         }
-            
+
         protected override void OnViewLoaded(object view)
-        {            
+        {
             base.OnViewLoaded(view);
         }
 
         protected override void OnActivate()
-        {            
+        {
             base.OnActivate();
         }
     }
