136beea2e95fd5cf96e3b690e2258a3937a5d3e3
Sebastian Wenninger <sebastian.wenninger@tum.de>
2015-10-12 11:03:06 +0200

    TableKombiView continued

--- TUM.TT/TT.Viewer/ViewModels/TableKombiViewModel.cs
+++ TUM.TT/TT.Viewer/ViewModels/TableKombiViewModel.cs
@@ -11,6 +11,24 @@ namespace TT.Viewer.ViewModels
     {
         private IEventAggregator events;
 
+        #region Properties
+        public bool ButtonsVisible { get; set; }
+
+
+
+        public Tuple<int,int> ButtonPosition
+        {
+            get { return (Tuple<int,int>)GetValue(ButtonPositionProperty); }
+            set { SetValue(ButtonPositionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ButtonPosition.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ButtonPositionProperty =
+            DependencyProperty.Register("ButtonPosition", typeof(Tuple<int,int>), typeof(TableKombiViewModel), null);
+
+        
+        #endregion
+
         public TableKombiViewModel(IEventAggregator eventAggregator)
         {
             events = eventAggregator;
