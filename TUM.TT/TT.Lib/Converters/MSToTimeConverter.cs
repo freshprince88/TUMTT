5e8f3d572face1f19521f0e807b8a3a4a1011507
Sebastian Wenninger <sebastian.wenninger@tum.de>
2016-02-22 15:17:08 +0100

    TimeConverter

---
+++ TUM.TT/TT.Lib/Converters/MSToTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TT.Lib.Converters
+{
+    public class MSToTimeConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double val = (double)value;
+            return TimeSpan.FromMilliseconds(val);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan span = (TimeSpan)value;
+            return span.TotalMilliseconds;
+        }
+    }
+}
