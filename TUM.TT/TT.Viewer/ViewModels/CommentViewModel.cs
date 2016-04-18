1673c2a72420e0a6cc0cb3efde57c910d38559cc
Michael Fuchs <fuchsmi@in.tum.de>
2016-04-18 17:56:06 +0200

    Comments added

---
+++ TUM.TT/TT.Viewer/ViewModels/CommentViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Caliburn.Micro;
+using GongSolutions.Wpf.DragDrop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TT.Lib.Events;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using TT.Lib.Managers;
+using TT.Lib.Models;
+
+namespace TT.Viewer.ViewModels
+{
+    public class CommentViewModel : Screen, IHandle<VideoPlayEvent>
+    {
+        private IEventAggregator events;
+        private IMatchManager Manager;
+        public Rally CurrentRally { get; set; }
+
+        public String Comment
+        {
+            get
+            {
+                return CurrentRally != null ? CurrentRally.Kommentar : string.Empty;
+            }
+            set
+            {
+                if (value != CurrentRally.Kommentar)
+                {
+                    CurrentRally.Kommentar = value;
+                    NotifyOfPropertyChange();
+                }
+            }
+        }
+
+        public CommentViewModel(IEventAggregator e, IMatchManager man)
+        {
+            events = e;
+            Manager = man;
+            
+        }
+
+        #region Event Handlers
+        public void Handle(VideoPlayEvent message)
+        {
+            CurrentRally = message.Current;
+            Comment = CurrentRally.Kommentar;
+            NotifyOfPropertyChange("Comment");
+        }
+        #endregion
+        #region Caliburn Hooks
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            // Subscribe ourself to the event bus
+            this.events.Subscribe(this);
+        }
+        #endregion
+
+
+
+
+
+    }
+}
