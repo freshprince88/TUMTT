b06302af59685991c3741ea92a4b1f2a4069bf5a
Niko <greece93@gmail.com>
2016-05-28 00:20:26 +0200

    Adds Spin to Stroke after Checking Radio Buttons in SpinRadioView.

--- TUM.TT/TT.Scouter/ViewModels/ServiceDetailViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/ServiceDetailViewModel.cs
@@ -5,16 +5,29 @@ namespace TT.Scouter.ViewModels
 {
     public class ServiceDetailViewModel : Conductor<IScreen>.Collection.AllActive
     {
-        public Schlag Stroke { get; set; }
+        private Schlag _stroke;
+        public Schlag Stroke {
+            get
+            {
+                return _stroke;
+            }
+            set
+            {
+                SpinControl.Stroke = value;
+                //TableControl.Stroke = value;
+                _stroke = value;
+            }
+        }
 
         public ServicePositionTableViewModel TableControl { get; set; }
         public SpinRadioViewModel SpinControl { get; set; }
 
         public ServiceDetailViewModel(Schlag s)
         {
-            Stroke = s;
             TableControl = new ServicePositionTableViewModel();
-            SpinControl = new SpinRadioViewModel();
+            SpinControl = new SpinRadioViewModel(s);
+
+            Stroke = s;
         }
 
         protected override void OnActivate()
--- TUM.TT/TT.Scouter/ViewModels/SpinRadioViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/SpinRadioViewModel.cs
@@ -1,8 +1,54 @@
 ﻿using Caliburn.Micro;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using TT.Models.Util.Enums;
 
 namespace TT.Scouter.ViewModels
 {
     public class SpinRadioViewModel : Screen
     {
+        public TT.Models.Schlag Stroke { get; set; }
+
+        public SpinRadioViewModel(TT.Models.Schlag s)
+        {
+            Stroke = s;
+        }
+
+        public void Checked(RoutedEventArgs e)
+        {
+            if(e.Source.GetType().Equals(typeof(RadioButton)))
+            {
+                RadioButton rb = (RadioButton)e.Source;
+                TT.Models.Spin newSpin = new TT.Models.Spin();
+
+                if (rb.Name.Contains("ÜS"))
+                    newSpin.ÜS = "1";
+                else
+                    newSpin.ÜS = "0";
+
+                if (rb.Name.Contains("US"))
+                    newSpin.US = "1";
+                else
+                    newSpin.US = "0";
+
+                if (rb.Name.Contains("SL"))
+                    newSpin.SL = "1";
+                else
+                    newSpin.SL = "0";
+
+                if (rb.Name.Contains("SR"))
+                    newSpin.SR = "1";
+                else
+                    newSpin.SR = "0";
+
+                if (rb.Name.Contains("No"))
+                    newSpin.No = "1";
+                else
+                    newSpin.No = "0";
+
+                Stroke.Spin = newSpin;
+            }
+        }
     }
 }
