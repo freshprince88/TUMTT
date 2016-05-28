7ce78c95f714442fdbcd18b105d0d15a7f5f2d3a
Niko <greece93@gmail.com>
2016-05-28 10:57:13 +0200

    Adds Platzierung to Stroke after Checking Radio Buttons in StrokePositionTableView.

--- TUM.TT/TT.Scouter/ViewModels/StrokePositionTableViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/StrokePositionTableViewModel.cs
@@ -4,5 +4,24 @@ namespace TT.Scouter.ViewModels
 {
     public class StrokePositionTableViewModel : Screen
     {
+        public Models.Schlag Stroke { get; set; }
+
+        public StrokePositionTableViewModel(Models.Schlag s)
+        {
+            Stroke = s;
+        }
+
+        public void ChangePositionPlayer(string position)
+        {
+            Stroke.Balltreffpunkt = position;
+        }
+
+        public void ChangePositionService(double X, double Y)
+        {
+            Models.Platzierung p = new Models.Platzierung();
+            p.WX = X;
+            p.WY = Y;
+            Stroke.Platzierung = p;
+        }
     }
 }
