2a95b878dfce0cec10d7c8f97fd9b0949e172a0b
Niko Tzioras <nikolaos.tzioras@tum.de>
2016-06-29 20:29:18 +0200

    Finally can move grid Item including Text

--- TUM.TT/TT.Scouter/ViewModels/RemotePositionsRallyViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/RemotePositionsRallyViewModel.cs
@@ -19,9 +19,9 @@ namespace TT.Scouter.ViewModels
 
         public string ToogleCalibrationButtonText { get; private set; }
 
-        private ObservableCollection<Ellipse> _drawnStrokes;
+        private ObservableCollection<DrawElement> _drawnStrokes;
 
-        public ObservableCollection<Ellipse> DrawnStrokes
+        public ObservableCollection<DrawElement> DrawnStrokes
         {
             get { return _drawnStrokes; }
             set
@@ -58,7 +58,7 @@ namespace TT.Scouter.ViewModels
             
             cal.StrokePositionCalculated += OnStrokePositionCalculated;
             ToogleCalibrationButtonText = "Hide Calibration";
-            DrawnStrokes = new ObservableCollection<Ellipse>();
+            DrawnStrokes = new ObservableCollection<DrawElement>();
         }
 
         protected override void OnActivate()
@@ -92,52 +92,58 @@ namespace TT.Scouter.ViewModels
             {
                 while (CurrentStroke.Nummer > DrawnStrokes.Count)
                 {
-                    DrawnStrokes.Add(createEllipseAtPosition(Visibility.Hidden));
+                    DrawnStrokes.Add(createDrawElementAtPosition(Visibility.Hidden));
                 }
             }
 
-            Ellipse e = DrawnStrokes[CurrentStroke.Nummer - 1];
-            putEllipseToPosition(args.Position, e);
-            e.Visibility = Visibility.Visible;
+            DrawElement dE = DrawnStrokes[CurrentStroke.Nummer - 1];
+            dE.text = CurrentStroke.Nummer.ToString();
+            putGridToPosition(args.Position, dE);
+            dE.g.Visibility = Visibility.Visible;
+            dE.e.Visibility = Visibility.Visible;
         }
 
-        private Ellipse putEllipseToPosition(Point Position, Ellipse e)
+        private DrawElement putGridToPosition(Point Position, DrawElement drawElement)
         {
             double x = Position.X * ((double)305 / 152.5);
             double y = Position.Y * ((double)548 / (double)274);
-            double left = x - (e.Width / 2);
-            double top = y - (e.Height / 2);
-            e.Margin = new Thickness(left, top, 0, 0);
+            double left = x - (drawElement.g.Width / 2);
+            double top = y - (drawElement.g.Height / 2);
+            drawElement.g.Margin = new Thickness(left, top, 0, 0);
 
-            return e;
+            return drawElement;
         }
 
-        private Ellipse createEllipseAtPosition(Visibility visibility)
+        private DrawElement createDrawElementAtPosition(Visibility visibility)
         {
             Ellipse e = new Ellipse();
-            e.Height = 30;
-            e.Width = 30;
             e.Stroke = System.Windows.Media.Brushes.Black;
             e.Fill = System.Windows.Media.Brushes.Transparent;
             e.StrokeThickness = 5;
             e.Visibility = visibility;
-            e.MouseDown += new System.Windows.Input.MouseButtonEventHandler(EllipseClicked);
-            e.MouseMove += new System.Windows.Input.MouseEventHandler(MouseMoved);
-            e.MouseUp += new System.Windows.Input.MouseButtonEventHandler(EllipseUnclicked);
-            return e;
+            Grid g = new Grid();
+            g.Height = 60;
+            g.Width = 60;
+            g.IsHitTestVisible = true;
+            g.Background = System.Windows.Media.Brushes.Transparent;
+
+            DrawElement ele = new DrawElement();
+            ele.e = e;
+            ele.g = g;
+            return ele;
         }
 
-        public void EllipseUnclicked(object sender, MouseButtonEventArgs e)
+        public void GridUnclicked(object sender, MouseButtonEventArgs e)
         {
             if (isEllipseDragged)
             {
                 isEllipseDragged = false;
-                Ellipse draggedEllipse = DrawnStrokes[CurrentStroke.Nummer - 1];
+                Grid draggedGrid = DrawnStrokes[CurrentStroke.Nummer - 1].g;
 
                 Platzierung p = new Platzierung();
                 // reversed the Method putEllipseToPosition()
-                double x = draggedEllipse.Margin.Left + (draggedEllipse.Width / 2);
-                double y = draggedEllipse.Margin.Top + (draggedEllipse.Height / 2);
+                double x = draggedGrid.Margin.Left + (draggedGrid.Width / 2);
+                double y = draggedGrid.Margin.Top + (draggedGrid.Height / 2);
                 p.WX = x / ((double)305/152.5);
                 p.WY = y / ((double)548 / (double)274);
                 CurrentStroke.Platzierung = p;
@@ -148,34 +154,37 @@ namespace TT.Scouter.ViewModels
         {
             if (isEllipseDragged == true)
             {
-                Ellipse el = DrawnStrokes[CurrentStroke.Nummer - 1];
+                Grid g = DrawnStrokes[CurrentStroke.Nummer - 1].g;
 
                 Point p = e.GetPosition((IInputElement)sender);
 
                 double x = p.X;
                 double y = p.Y;
-                double left = x - (el.Width / 2);
-                double top = y - (el.Height / 2);
-                el.Margin = new Thickness(left, top, 0, 0);
+                double left = x - (g.Width / 2);
+                double top = y - (g.Height / 2);
+                g.Margin = new Thickness(left, top, 0, 0);
             }
         }
 
-        private void EllipseClicked(object sender, MouseButtonEventArgs e)
+        public void GridClicked(object sender, MouseButtonEventArgs e)
         {
-            Ellipse ellipse = sender as Ellipse;
-            isEllipseDragged = true;
-
-            // Point p = e.GetPosition(ellipse);
-            // here was some offset - setting - didn't work so i deleted it xD
-
-            for (int i = 0; i < DrawnStrokes.Count; i++)
+            Grid grid = sender as Grid;
+            string number = "";
+            foreach (Object t in grid.Children)
             {
-                if (DrawnStrokes[i].Equals(ellipse))
-                {
-                    CurrentStroke = Strokes[i];
-                }
+                if (t.GetType() == typeof(TextBlock)) number = ((TextBlock)t).Text;
             }
+
+            CurrentStroke = Strokes[int.Parse(number) - 1];
+            isEllipseDragged = true;
         }
 
+    }
+
+    public class DrawElement
+    {
+        public Grid g { get; set; }
+        public Ellipse e { get; set; }
+        public string text { get; set; }
     }
 }
--- TUM.TT/TT.Scouter/Views/RemotePositionsRallyView.xaml.cs
+++ TUM.TT/TT.Scouter/Views/RemotePositionsRallyView.xaml.cs
@@ -24,5 +24,10 @@ namespace TT.Scouter.Views
         {
             InitializeComponent();
         }
+
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
