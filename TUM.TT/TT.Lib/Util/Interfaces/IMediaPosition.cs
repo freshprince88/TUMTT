149e8d4a5f5a0b3a28df8910a3a9b02506d93984
Niko Tzioras <nikolaos.tzioras@tum.de>
2016-07-08 22:57:30 +0200

    Somehow this got ignored..?

---
+++ TUM.TT/TT.Lib/Util/Interfaces/IMediaPosition.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+
+namespace TT.Lib.Interfaces
+{
+    public interface IMediaPosition : IScreen
+    {
+        TimeSpan MediaPosition { get; set; }
+
+        TimeSpan EndPosition { get; set; }
+
+        double Minimum { get; set; }
+
+        double Maximum { get; set; }
+
+        bool toRallyStart { get; set; }
+
+        void Play();
+        
+        void Pause();
+
+        void Stop();
+
+        
+    }
+}
