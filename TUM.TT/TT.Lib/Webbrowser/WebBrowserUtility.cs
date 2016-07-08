71f26070a6f1a1f20bb8f60d77c57a88ead96895
Niko Tzioras <nikolaos.tzioras@tum.de>
2016-07-08 23:01:45 +0200

    Still more files missing...

---
+++ TUM.TT/TT.Lib/Webbrowser/WebBrowserUtility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TT.Lib.Webbrowser
+{
+    public class WebBrowserUtility
+    {
+        public static readonly DependencyProperty BindableSourceProperty = DependencyProperty.RegisterAttached("BindableSource", typeof(string), typeof(WebBrowserUtility), new UIPropertyMetadata(null, OnBindableSourceChanged));
+
+        public static string GetBindableSource(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BindableSourceProperty);
+        }
+
+        public static void SetBindableSource(DependencyObject obj, string value)
+        {
+            obj.SetValue(BindableSourceProperty, value);
+        }
+
+        private static void OnBindableSourceChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var browser = o as WebBrowser;
+            if (browser == null)
+                return;
+
+            var uri = (string)e.NewValue;
+
+            try
+            {
+                browser.Source = !string.IsNullOrEmpty(uri) ? new Uri(uri) : null;
+            }
+            catch (ObjectDisposedException) { }
+        }
+    }
+}
