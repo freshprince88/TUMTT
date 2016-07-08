bbf083e64a3af8d63a92562a30fdd89f4928e6dc
Niko Tzioras <nikolaos.tzioras@tum.de>
2016-07-09 00:40:18 +0200

    Added PointOfContact awareness

--- TUM.TT/TT.Scouter/ViewModels/StrokePositionTableViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/StrokePositionTableViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using Caliburn.Micro;
 
 namespace TT.Scouter.ViewModels
 {
@@ -6,15 +7,82 @@ namespace TT.Scouter.ViewModels
     {
         public Models.Stroke Stroke { get; set; }
 
+        private bool _over;
+        public bool over
+        {
+            get { return _over; }
+            set
+            {
+                _over = value;
+                NotifyOfPropertyChange("over");
+            }
+        }
+
+        private bool _half;
+        public bool half
+        {
+            get { return _half; }
+            set
+            {
+                _half = value;
+                NotifyOfPropertyChange("half");
+            }
+        }
+
+        private bool _behind;
+        public bool behind
+        {
+            get { return _behind; }
+            set
+            {
+                _behind = value;
+                NotifyOfPropertyChange("behind");
+            }
+        }
+
+
         public StrokePositionTableViewModel(Models.Stroke s)
         {
             Stroke = s;
+            _over = false;
+            half = false;
+            _behind = false;
+            if (s.PointOfContact == null)
+                s.PointOfContact = "";
+
+            if (s.PointOfContact.Equals("over"))
+                _over = true;
+            else if (s.PointOfContact.Equals("half-distance"))
+                half = true;
+            else if (s.PointOfContact.Equals("behind"))
+                _behind = true;
         }
 
-        public void ChangePositionPlayer(string position)
+        public void ChangePointOfContact(string pointOfContact)
         {
-            Stroke.PlayerpositionString = position;
+            if (Stroke.PointOfContact.Equals(pointOfContact))
+            {
+                if (pointOfContact.Equals("over"))
+                    over = !over;
+                else if (pointOfContact.Equals("half-distance"))
+                    half = !half;
+                else if (pointOfContact.Equals("behind"))
+                    behind = !behind;
+            }
+            OnPointOfContactChange();
         }
+        private void OnPointOfContactChange()
+        {
+            if (over)
+                Stroke.PointOfContact = "over";
+            else if (half)
+                Stroke.PointOfContact = "half-distance";
+            else if (behind)
+                Stroke.PointOfContact = "behind";
+            else
+                Stroke.PointOfContact = "";
+        }
+
 
         public void ChangePositionStroke(double X, double Y)
         {
