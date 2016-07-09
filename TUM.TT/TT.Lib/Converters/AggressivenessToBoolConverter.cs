8a9d1f6ba60d3864be0252c2fbe6041a345ad684
Niko <nikolaos.tzioras@tum.de>
2016-07-09 00:36:45 +0000

    Fix by hand

---
+++ TUM.TT/TT.Lib/Converters/AggressivenessToBoolConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using TT.Models;
+using TT.Models.Util.Enums;
+
+namespace TT.Lib.Converters
+
+{
+    [ValueConversion(typeof(HashSet<Models.Util.Enums.Stroke.Services>), typeof(bool))]
+    public class AggressivenessToBoolConverter : BaseConverter, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            HashSet<Models.Util.Enums.Stroke.Aggressiveness> set = (HashSet<Models.Util.Enums.Stroke.Aggressiveness>)values[0];
+            string btnName = (string)values[1];
+
+            foreach (var agg in set)
+            {
+                var test = agg.ToString().ToLower();
+                if (test == btnName.ToLower())
+                    return true;
+            }
+            return false;
+        }
+
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+
