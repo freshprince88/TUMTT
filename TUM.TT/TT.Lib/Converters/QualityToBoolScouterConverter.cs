494ae19f84a339b82568379b3e362484418ae641
Niko <nikolaos.tzioras@tum.de>
2016-07-09 00:41:55 +0000

    Upload new file

---
+++ TUM.TT/TT.Lib/Converters/QualityToBoolScouterConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using TT.Models.Util.Enums;
+
+namespace TT.Lib.Converters
+{
+
+    public class QualityToBoolScouterConverter : BaseConverter, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] != null)
+            {
+                string quality = (string)values[0];
+                string btnName = (string)values[1];
+                return quality.ToLower() == btnName.ToLower();
+            }
+            return false;
+
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
