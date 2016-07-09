c8475a925010d7403c338f12bc646c57ac23ba91
Niko <nikolaos.tzioras@tum.de>
2016-07-09 00:44:06 +0000

    Upload new file

---
+++ TUM.TT/TT.Lib/Converters/TwoBoolToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using TT.Models.Util.Enums;
+
+namespace TT.Lib.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class TwoBoolToBoolConverter : BaseConverter, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool Player1TopPlayer2Bottom = (bool)values[0];
+            bool Player2TopPlayer1Bottom = (bool)values[1];
+
+            return Player1TopPlayer2Bottom || Player2TopPlayer1Bottom;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
