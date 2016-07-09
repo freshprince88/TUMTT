b76165145407f9abd6dcc884168c0a35e5faa7eb
Niko <nikolaos.tzioras@tum.de>
2016-07-09 00:46:04 +0000

    Upload new file

---
+++ TUM.TT/TT.Lib/Events/PlayModeEvent.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TT.Models.Util.Enums;
+
+namespace TT.Lib.Events
+{
+    public class PlayModeEvent
+    {
+        public bool? PlayMode { get; set; }
+
+        public PlayModeEvent(bool? pm)
+        {
+            PlayMode = pm;
+        }
+
+    }
+}
