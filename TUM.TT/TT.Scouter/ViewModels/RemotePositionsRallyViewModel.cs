b01c3812cc1f2d74567e24319e2d6255d82137a4
Michael Fuchs <fuchsmi@in.tum.de>
2019-01-08 14:58:38 +0100

    Revert "change Strokes vs DrawnStrokes fix."
    This reverts commit 196c4a8879abd39d97a4814de6e749613a0926ee.

--- TUM.TT/TT.Scouter/ViewModels/RemotePositionsRallyViewModel.cs
+++ TUM.TT/TT.Scouter/ViewModels/RemotePositionsRallyViewModel.cs
@@ -444,8 +444,7 @@ namespace TT.Scouter.ViewModels
 
         private void showCorrectStrokes()
         {
-            if (Strokes.Count == DrawnStrokes.Count) { 
-            for (int i = 0; i < Strokes.Count; i++)
+            for (int i = 0; i < DrawnStrokes.Count; i++)
             {
                 if (Math.Abs((CurrentStroke.Number - 1 - i)) <= (_maxVisibleStrokes - 1) / 2)
                 {
@@ -455,7 +454,6 @@ namespace TT.Scouter.ViewModels
                 {
                     DrawnStrokes[i].g.Visibility = Visibility.Hidden;
                 }
-            }
             }
         }
 
